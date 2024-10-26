fc2e36159a78692dcf3a923a132f23691d93821e
Quantumapple <jongapples@gmail.com>
2024-10-26 17:02:06 -0500

    input argu for x509 proxy

--- step2/submit.py
+++ step2/submit.py
@@ -28,7 +28,8 @@ def make_jobs(args, base_dir):
 
     jdl = """universe              = vanilla
 executable            = {1}
-arguments             = $(path) $(fname)
+Proxy_path            = {3}
+arguments             = $(path) $(fname) $(Proxy_path)
 should_Transfer_Files = YES
 transfer_input_files  = {2}
 transfer_output_files = ""
@@ -38,7 +39,7 @@ log                   = {0}/$(ClusterId).$(ProcId).log
 MY.SingularityImage   = "/cvmfs/unpacked.cern.ch/gitlab-registry.cern.ch/batch-team/containers/plusbatch/el7-full:latest"
 +JobFlavour           = "tomorrow"
 queue path,fname from input_list.txt
-""".format(log_dir.name, f'run_MC_{args.year}.sh', args.hadronizer)
+""".format(log_dir.name, f'run_MC_{args.year}.sh', args.hadronizer, args.proxypath)
 
     with open(f'condor_MC.jdl','w') as jdlfile:
         jdlfile.write(jdl)
@@ -59,9 +60,9 @@ if __name__ == "__main__":
 
     parser.add_argument("--path",       dest="path",        required = True,  help="Input path to LHE files", type=str)
     parser.add_argument("--hadronizer", dest="hadronizer",  required = True,  help="Hadronizer file")
+    parser.add_argument("--proxypath",  dest="proxypath",   required = True,  help="Full AFS path to your x509 proxy", type=str)
     parser.add_argument("--nevt",       dest="nevt",        default=500,      help="Number of events to produce, default = 500", type=int)
     parser.add_argument("--year",       dest="year",        default=2018,     help="Year for MC production")
-
     parser.add_argument("--dryrun", dest="dryrun", action="store_true", help="Print bash, and jdl instead of submitting job")
 
     args = parser.parse_args()
