d0e5f0b05c80c20fea2a918324bbf717d06ce4c0
CodexForster <dsheka3@uic.edu>
2026-01-01 21:16:22 -0600

    2022 MC campaign paths/parameters.

--- step2/README.md
+++ step2/README.md
@@ -1,20 +1,22 @@
-## Before submitting condor jobs
-https://batchdocs.web.cern.ch/local/myschedd.html
+## Generate x509 proxy and move to your lxplus private area
+Initiate voms with a command like: voms-proxy-init --rfc --voms cms -valid 192:00.
+Resources: https://www.hep.wisc.edu/cms/comp/basicgrid.html, https://batchdocs.web.cern.ch/tutorial/exercise2e_proxy.html.
 
-This can help user jumping to the schedd that has less jobs.
-`myschedd bump`
+## Submitting jobs via submit.py
+Update the relevant paths and MC parameters in run_MC_202X.sh and submit.py.
 
-## Move your x509 proxy to your lxplus private area
-https://batchdocs.web.cern.ch/tutorial/exercise2e_proxy.html
+usage: python3 submit.py
 
-## How to run
-usage: submit.py [-h] --path PATH --hadronizer HADRONIZER --proxypath PROXYPATH --eospath EOSPATH [--nevt NEVT] [--year YEAR] [--dryrun]
+Please note that the 2022 job submission uses the fragment file used by the central MC campaign from https://cms-pdmv-prod.web.cern.ch/mcm/public/restapi/requests/get_test/HIG-Run3Summer22wmLHEGS-02385.
+
+## Using longsubmit.py job file [DEPRECIATED]
+usage: longsubmit.py [-h] --path PATH --hadronizer HADRONIZER --proxypath PROXYPATH --eospath EOSPATH [--nevt NEVT] [--year YEAR] [--dryrun]
 
 Example:
 
-python3 submit.py --path /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/vbfhToWW2L2Nu/CMSSW_13_0_13/src/genproductions_scripts/bin/Powheg/VBF_H_el8_amd64_gcc11_CMSSW_13_0_13_VBFHToWW2L2Nu.tgz --hadronizer /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/CMSSW_13_0_13/src/Configuration/GenProduction/python/vbf_h_ww_2l2Nu_el8_amd64_gcc11_CMSSW_13_0_13-fragment.py --proxypath /afs/cern.ch/user/d/dshekar/private/x509up_u154072 --eospath root://eosuser.cern.ch//eos/user/d/dshekar/mcSample/ --backup https://cernbox.cern.ch/files/spaces/eos/user/d/dshekar/mcSample/ --nevt 10 --year 2023 --pyedits /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/mcSampleProdScripts/step2/update_paths.py  --premix_file_path /eos/cms/store/mc/Run3Summer21PrePremix/Neutrino_E-10_gun/PREMIX/Summer23_130X_mcRun3_2023_realistic_v13-v1/
+python3 longsubmit.py --path /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/vbfhToWW2L2Nu/CMSSW_13_0_13/src/genproductions_scripts/bin/Powheg/VBF_H_el8_amd64_gcc11_CMSSW_13_0_13_VBFHToWW2L2Nu.tgz --hadronizer /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/CMSSW_13_0_13/src/Configuration/GenProduction/python/vbf_h_ww_2l2Nu_el8_amd64_gcc11_CMSSW_13_0_13-fragment.py --proxypath /afs/cern.ch/user/d/dshekar/private/x509up_u154072 --eospath root://eosuser.cern.ch//eos/user/d/dshekar/mcSample/ --backup https://cernbox.cern.ch/files/spaces/eos/user/d/dshekar/mcSample/ --nevt 10 --year 2023 --pyedits /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/mcSampleProdScripts/step2/update_paths.py  --premix_file_path /eos/cms/store/mc/Run3Summer21PrePremix/Neutrino_E-10_gun/PREMIX/Summer23_130X_mcRun3_2023_realistic_v13-v1/
 
-## I think incorrect: python3 submit.py --path ./ --hadronizer /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/CMSSW_13_0_13/src/Configuration/GenProduction/python/vbf_h_ww_2l2Nu_el8_amd64_gcc11_CMSSW_13_0_13-fragment.py --proxypath /afs/cern.ch/user/d/dshekar/private/x509up_u154072 --eospath root://eosuser.cern.ch//eos/user/d/dshekar/mcSample/ --nevt 10 --year 2023
+# I think incorrect: python3 longsubmit.py --path ./ --hadronizer /afs/cern.ch/user/d/dshekar/private/mcSamples/powheg/CMSSW_13_0_13/src/Configuration/GenProduction/python/vbf_h_ww_2l2Nu_el8_amd64_gcc11_CMSSW_13_0_13-fragment.py --proxypath /afs/cern.ch/user/d/dshekar/private/x509up_u154072 --eospath root://eosuser.cern.ch//eos/user/d/dshekar/mcSample/ --nevt 10 --year 2023
 
 options:
 *  -h, --help              show this help message and exit
---
+++ step2/run_MC_2022.sh
@@ -0,0 +1,73 @@
+#!/bin/bash
+### Computing environments
+echo "Starting job on " `date` #Date/time of start of job
+echo "Running on: `uname -a`" #Condor job is running on this node
+source /cvmfs/cms.cern.ch/cmsset_default.sh
+export BASEDIR=`pwd`
+echo "basedir = ${BASEDIR}"
+
+### Add x509 proxy
+export X509_USER_PROXY=${3}
+voms-proxy-info -all
+voms-proxy-info -all -file ${3}
+
+echo "DAN "
+echo "Argument 0: ${0}" # run_MC_2023.sh file itself
+echo "Argument 1: ${1}" # ClusterID file
+echo "Argument 2: ${2}" # ProcID file
+echo "Argument 3: ${3}" # Proxy file name
+CLUSTER_ID=${1}
+PROC_ID=${2}
+
+### LHEGS step
+echo "********** lhegs start **********"
+export SCRAM_ARCH=el8_amd64_gcc11
+scram p CMSSW_13_0_13
+cd CMSSW_13_0_13/src
+eval `scram runtime -sh`
+
+mkdir -p Configuration/GenProduction/python/
+mv ${BASEDIR}/HIG-Run3Summer22wmLHEGS-02385-fragment.py Configuration/GenProduction/python/
+scram b
+SEED=$((${1} + ${2}))
+echo "Seed set to: ${SEED}"
+cmsDriver.py Configuration/GenProduction/python/HIG-Run3Summer22wmLHEGS-02385-fragment.py --era Run3 --customise Configuration/DataProcessing/Utils.addMonitoring --beamspot Realistic25ns13p6TeVEarly2022Collision --step LHE,GEN,SIM --geometry DB:Extended --conditions 124X_mcRun3_2022_realistic_v12 --customise_commands process.RandomNumberGeneratorService.externalLHEProducer.initialSeed="int(${SEED})"\\nprocess.source.numberEventsInLuminosityBlock="cms.untracked.uint32(100)" --datatier GEN-SIM,LHE --eventcontent RAWSIM,LHE --python_filename wmLHEGS_cfg.py --fileout file:zh_hto2w_zto2l_LHEGS.root --mc --no_exec -n 5000
+cmsRun wmLHEGS_cfg.py
+echo "********** LHE End **********"
+echo "Files in current directory: "
+echo $(ls)
+
+### DIGI-Premix step
+echo "********** DIGI Premix start **********"
+cmsDriver.py --python_filename DRPremix_cfg.py --mc --eventcontent PREMIXRAW --customise Configuration/DataProcessing/Utils.addMonitoring --datatier GEN-SIM-RAW --conditions 124X_mcRun3_2022_realistic_v12  --fileout file:zh_hto2w_zto2l_DRPremix.root --pileup_input "dbs:/Neutrino_E-10_gun/Run3Summer21PrePremix-Summer22_124X_mcRun3_2022_realistic_v11-v2/PREMIX" --step DIGI,DATAMIX,L1,DIGI2RAW,HLT:2022v12 --procModifiers premix_stage2,siPixelQualityRawToDigi --nThreads 4 --geometry DB:Extended --datamix PreMix --era Run3  --filein file:zh_hto2w_zto2l_LHEGS.root --no_exec -n 5000
+find /eos/cms/store/mc/Run3Summer21PrePremix/Neutrino_E-10_gun/PREMIX/Summer22_124X_mcRun3_2022_realistic_v11-v2/ -type f > find_query_list_test.txt
+sed -i "s|/eos/cms/|/|g; s|root$|root|g" find_query_list_test.txt
+mv ${BASEDIR}/update_paths.py ./
+python3 update_paths.py
+cmsRun DRPremix_cfg.py
+echo "********** DIGI Premix End **********"
+xrdcp -f DRPremix_cfg.py root://eosuser.cern.ch//eos/user/d/dshekar/mcSample/DRPremix_cfg.py
+
+### AOD step
+echo "********** AOD start **********"
+cmsDriver.py --python_filename aod_cfg.py --eventcontent AODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier AODSIM --conditions 124X_mcRun3_2022_realistic_v12 --step RAW2DIGI,L1Reco,RECO,RECOSIM --procModifiers siPixelQualityRawToDigi --nThreads 4 --geometry DB:Extended --era Run3 --filein file:zh_hto2w_zto2l_DRPremix.root --fileout file:zh_hto2w_zto2l_AOD.root --no_exec --mc -n 5000
+cmsRun aod_cfg.py
+echo "********** AOD End **********"
+
+### MINIAOD step
+echo "********** MINIAOD start **********"
+cmsDriver.py --python_filename miniAOD_cfg.py --eventcontent MINIAODSIM --customise Configuration/DataProcessing/Utils.addMonitoring --datatier MINIAODSIM --conditions 124X_mcRun3_2022_realistic_v12 --step PAT --nThreads 2 --geometry DB:Extended --era Run3 --filein file:zh_hto2w_zto2l_AOD.root --fileout file:zh_hto2w_zto2l_miniAOD.root --no_exec --mc -n 5000
+cmsRun miniAOD_cfg.py
+echo "********** MINIAOD End **********"
+
+### NANOAOD step
+echo "********** NANOAOD start **********"
+cmsDriver.py --python_filename nanoAOD_cfg.py --eventcontent NANOAODSIM --datatier NANOAODSIM --conditions 126X_mcRun3_2022_realistic_v2 --step NANO --nThreads 4 --scenario pp --era Run3,run3_nanoAOD_124  --filein file:zh_hto2w_zto2l_miniAOD.root --fileout file:zh_hto2w_zto2l_nanoAOD.root --no_exec --mc -n 5000
+cmsRun nanoAOD_cfg.py
+echo "********** NANOAOD End **********"
+
+FINAL_FILE_NAME="nanoAOD_${CLUSTER_ID}_${PROC_ID}.root"
+mv zh_hto2w_zto2l_nanoAOD.root ${FINAL_FILE_NAME}
+
+# Copy the renamed file to the destination
+xrdcp -f ${FINAL_FILE_NAME} root://eosuser.cern.ch//eos/user/d/dshekar/MCsims_ZH3l/mcSample_ZH_HWW_Z2L_centralSampleGridpack/${FINAL_FILE_NAME}
